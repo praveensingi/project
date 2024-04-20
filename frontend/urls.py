ccda965104465bad8f5c320efefe241d7d7834ac
Vinesh Reddy Borra <vineshreddy@Vineshs-MacBook-Air.local>
2024-04-19 21:45:34 -0500

    draft version

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('careers/', views.careers, name='careers'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def careers(request):
+    return render(request, 'careers.html')
